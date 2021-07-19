773a42dc431f537156310491995189b73287d7a3
Rostyslav <rklyba@yahoo.com>
2021-07-19 13:50:27 +0300

    Configured Toolbox controls

--- 22 - Drawing App/script.js
+++ 22 - Drawing App/script.js
@@ -1,26 +1,32 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
-let size = 20;
+const ctx = canvas.getContext("2d");
+
+let size = 10;
 let isPressed = false;
-let color = 'black';
+let color = "black";
 let x;
 let y;
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
   x = e.offsetX;
   y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', (e) => {
+canvas.addEventListener("mouseup", (e) => {
   isPressed = false;
   x = undefined;
   y = undefined;
 });
 
-canvas.addEventListener('mousemove', (e) => {
-  if(isPressed) {
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
     drawCircle(x2, y2);
@@ -31,7 +37,7 @@ canvas.addEventListener('mousemove', (e) => {
   }
 });
 
-function drawCircle (x, y) {
+function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fillStyle = color;
@@ -47,3 +53,21 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
 }
 
+increaseBtn.addEventListener("click", () => {
+  size++;
+  sizeEl.innerText = size;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size--;
+  if (size < 1) {
+    size = 1;
+  }
+  sizeEl.innerText = size;
+});
+
+colorEl.addEventListener("change", (e) => (color = e.target.value));
+
+clearEl.addEventListener("click", () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
